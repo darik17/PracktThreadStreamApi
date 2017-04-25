e4b073f7a4430852308446efa569136108bb809c
darik17 <darik17@yandex.ru>
2017-04-25 22:41:16 +0300

    переделывал дома

--- src/innopolis/MainClass.java
+++ src/innopolis/MainClass.java
@@ -2,23 +2,72 @@ package innopolis;
 
 import org.apache.log4j.Logger;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * Created by admin on 24.04.2017.
+ * Необходимо разработать программу, которая получает на вход список ресурсов, содержащих набор
+ * чисел и считает сумму всех положительных четных. Каждый ресурс должен быть обработан в отдельном
+ * потоке, набор должен содержать лишь числа, унарный оператор "-" и пробелы. Общая сумма должна
+ * отображаться на экране и изменяться в режиме реального времени.
+ * Запуск потоков реализовать через ссылки на методы, итоговый подсчет суммы через stream API
  */
 public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class);
-    static int
-
+    private static String path = "C:\\Users\\admin\\IdeaProjects\\Prackt Thread Stream Api\\src\\testFiles\\";
+    static Collection<Integer> list = new CopyOnWriteArrayList<>();
     public static void main(String[] args) {
         LOGGER.info("Начало");
+        Integer sum;
         File folder = new File("src/testFiles");
         String[] files = folder.list();
 
-        for (int i = 0; i<files.length; i++){
-            ThreadRead threadRead = new ThreadRead(files[i]);
-            threadRead.run();
+        for (int i = 0; i < files.length; i++) {
+            final String nameFile = files[i];
+
+
+            new Thread(
+                    () -> readFile(nameFile)
+            ).start();
+
         }
+
+        //list.stream().reduce((s1,s2)->(s1+s2)).orElse(0);
+        sum = list.stream().reduce((s1,s2)->s1+s2).orElse(0);
+        System.out.println("Length: "+list.size());
+        System.out.println("Summa:"+sum);
+    }
+
+    public static void readFile(String name) {
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path + name));) {
+
+            String tempStr = null;
+            int tempInt = 0;
+            while ((tempStr = reader.readLine()) != null) {
+                String[] masStr = tempStr.split(" ");
+
+                // int[] masChisel = new int[masStr.length];
+                for (int i = 0; i < masStr.length; i++) {
+
+                    if ((tempInt = Integer.parseInt(masStr[i])) > 0) {
+                        list.add(tempInt);
+                        System.out.println(tempInt);
+                    }
+                }
+                LOGGER.info("!!!!");
+            }
+        } catch (FileNotFoundException e) {
+            LOGGER.error("Ошибка чтения файла");
+        } catch (IOException e) {
+            LOGGER.error("Ошибка чтения ");
+        }
+
+
+
     }
 }
--- src/innopolis/ThreadRead.java
+++ src/innopolis/ThreadRead.java
@@ -5,12 +5,14 @@ import org.apache.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by admin on 24.04.2017.
  */
 public class ThreadRead implements Runnable {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class);
+    private String path = "C:\\Users\\admin\\IdeaProjects\\Prackt Thread Stream Api\\src\\testFiles\\";
     private String fileName;
     Integer sumF;
 
@@ -20,11 +22,16 @@ public class ThreadRead implements Runnable {
 
     @Override
     public void run() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            sumF = 
-        } catch (FileNotFoundException e) {
-            LOGGER.error("Ошибка чтения");
+        try (BufferedReader reader = new BufferedReader(new FileReader(path+fileName));){
+
+            String temp = null;
+            while ((temp=reader.readLine())!=null) {
+                String[] mas =temp.split(" ");
+            }
+        } catch (FileNotFoundException  e) {
+            LOGGER.error("Ошибка чтения файла");
+        } catch (IOException e) {
+            LOGGER.error("Ошибка чтения ");
         }
 
         LOGGER.info(getClass().toString());
